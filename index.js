01a1ec34346b3e13787f03cd492a79618c08b4c2
Sonia Chevli <soniachevli@gmail.com>
2018-10-03 11:16:51 -0700

    change listening port

--- index.js
+++ index.js
@@ -31,4 +31,4 @@ app.use("/api/photos", photos);
 
 const port = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
